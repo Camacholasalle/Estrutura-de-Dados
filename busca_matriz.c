06875aab6dffdabc7f688f04f9e711b27657ddc0
Lucas Camacho <32284738+Camacholasalle@users.noreply.github.com>
2018-08-28 10:26:20 -0300

    Update busca_matriz.c

--- busca_matriz.c
+++ busca_matriz.c
@@ -1,80 +1,59 @@
-/*MATRIZ*/
+#
+include < stdio.h >
 
-#include<stdio.h>
+  int main(void) {
 
-int main(){
+    int M[3][3];
+    int num, i, j, opc;
 
-int i, j, num, n=3;
-int M[3][3];
+    printf("\n******************************** Matriz do Lucas *******************************");
+    do {
+      //Ler a Matriz
+      for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+          printf("\n\nDigite o termo [%d]-[%d] da matriz: ", i, j);
+          scanf("%d", & M[i][j]);
+        }
 
-printf("\n\n********************* MATRIZ DO Camacho el Cangaceiro **************************\n\n\n\n");
-//Faz a leitura da matriz M
+      }
 
-for(i=0; i<n; i++){
-    for(j=0; j<n; j++){
-        printf("Digite o elemento [%d]-[%d] da matriz: ", i,j);
-        scanf("%d", &M[i][j]);
-    }
-}
+      //Exibir a Matriz
 
-//Mostrar a Matriz
-
-printf("\nA Matriz e:\n");
-    for(i=0; i<n; i++){
-        for(j=0; j<n; j++){
-            printf("%6d", M[i][j]);
+      printf("\n A Matriz e: \n\n");
+      for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+          printf("%6d", M[i][j]);
         }
         printf("\n");
+      }
+
+      printf("\nEntre com o numero de busca ");
+      scanf("%d", & num);
+      busca(M, num);
+      printf("\n");
+
+      printf("Deseja fazer outra Matriz? [1]SIM || [2]NAO. ");
+      scanf("%d", & opc);
+
+    } while (opc == 1);
+
+    system("pause");
+    return 0;
+  }
+
+void busca(int M[][3], int num) {
+  int i, j, achou;
+
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      if (M[i][j] == num) {
+        printf("\nFoi encontrado na posicao [%d]-[%d] da Matriz", i, j);
+        achou = 1;
+      }
     }
-
-
-//Solicita o elemento de busca
-
-printf("\nEntre com o numero de busca ");
-scanf("%d", &num);
-busca(M, num);
-printf("\n");
-
-
-system("pause");
-return 0;
+  }
+  if (achou != 1) {
+    printf("Esse item nao se encontra na matriz. ");
+  }
 
 }
-
-void busca(int M[][3], int num){
-    int i,j, achou;
-
-for(i=0; i<3; i++){
-    for(j=0; j<3; j++){
-        if(M[i][j]==num){
-            printf("\nFoi encontrado na posicao [%d]-[%d] da Matriz",i, j);
-            achou=1;
-        }
-    }
-}
-if(achou==0){
-        printf("Não tem playboy");
-}
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
